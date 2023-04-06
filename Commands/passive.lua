073c9c3f2025d91af44a2ae9c8323676c958f2c6
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-06 21:08:18 +0200

    Update passive.lua

--- Commands/passive.lua
+++ Commands/passive.lua
@@ -1,1 +1,12 @@
+local Character = game.Players.LocalPlayer.Character
 
+local PhysResist = Character.Stats:FindFirstChild("Phys-Resist")
+local KiResist = Character.Stats:FindFirstChild("Ki-Resist")
+
+if PhysResist then
+	PhysResist:Destroy()
+end
+
+if KiResist then
+	KiResist:Destroy()
+end
