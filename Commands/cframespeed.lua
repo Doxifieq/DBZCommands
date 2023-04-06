acff1491f4102ee186b12ee2a7d487e6bc97dd73
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-06 20:48:34 +0200

    Update cframespeed.lua

--- Commands/cframespeed.lua
+++ Commands/cframespeed.lua
@@ -5,11 +5,11 @@ local CFrameSpeed = function()
 	local Vector = Vector3.new(0, 0, -15)
 
 	if (getgenv().GlobalSettings.Booleans.HoldingLeftControl) then
-		Vector = Vector3.new(0, -10, -8.5)
+		Vector = Vector3.new(0, -13, -8.5)
 	end
 
 	if (getgenv().GlobalSettings.Booleans.HoldingSpace) then
-		Vector = Vector3.new(0, 10, -8.5)
+		Vector = Vector3.new(0, 13, -8.5)
 	end
 
 	if (game.Players.LocalPlayer.Character) then
