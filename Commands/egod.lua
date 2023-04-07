4f5a138b9fc6d0de0df690ca1b58e5f1a4767678
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-07 15:37:25 +0200

    Update egod.lua

--- Commands/egod.lua
+++ Commands/egod.lua
@@ -1,1 +1,14 @@
+local RunService = game:GetService("RunService")
 
+table.insert(getgenv().GlobalSettings.Connections, RunService.Stepped:Connect(function()
+	print("RUNNING")
+	local Character = game.Players.LocalPlayer.Character
+
+	if Character then
+		local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+
+		if Humanoid and Humanoid.Health > 0 then
+			firetouchinterest(workspace.Touchy.Part, 0)
+		end
+	end
+end))
