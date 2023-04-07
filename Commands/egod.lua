c69d2143e16a5ddfd83daaae9ac6fe89322f5b77
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-07 15:48:33 +0200

    Update egod.lua

--- Commands/egod.lua
+++ Commands/egod.lua
@@ -1,14 +1,23 @@
 local RunService = game:GetService("RunService")
 
+local Character = game.Players.LocalPlayer.Character
+local Root = Character:FindFirstChild("HumanoidRootPart")
+
 table.insert(getgenv().GlobalSettings.Connections, RunService.Stepped:Connect(function()
 	print("RUNNING")
-	local Character = game.Players.LocalPlayer.Character
+	Character = game.Players.LocalPlayer.Character
 
 	if Character then
-		local Root = Character:FindFirstChild("HumanoidRootPart")
+		Root = Character:FindFirstChild("HumanoidRootPart")
 
 		if Root then
 			firetouchinterest(workspace.Touchy.Part, Root, 0)
 		end
+	end
+end))
+
+table.insert(getgenv().GlobalSettings.Connections, workspace.Touchy.Part.TouchEnded:Connect(function(Instance)
+	if string.match(Instance.Parent.Name, game.Players.LocalPlayer.Name) then
+		firetouchinterest(workspace.Touchy.Part, Root, 0)
 	end
 end))
