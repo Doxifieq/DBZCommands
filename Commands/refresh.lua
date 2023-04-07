77f4f87582f137b7b52fd962d0a80c9c4fe230d1
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-07 22:49:24 +0200

    Update refresh.lua

--- Commands/refresh.lua
+++ Commands/refresh.lua
@@ -1,1 +1,21 @@
+local ChatStart = game.Players.LocalPlayer.Backpack.ServerTraits.ChatStart
+local ChatAdvance = game.Players.LocalPlayer.Backpack.ServerTraits.ChatAdvance
+local ChatUI = game.Players.LocalPlayer.PlayerGui.HUD.Bottom.ChatGui
+local HairScript
 
+local formerPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+
+local repeatAdvance = function(Text, Parameter)
+	repeat task.wait(.15)
+		ChatAdvance:FireServer(Parameter)
+	until ChatUI.TextLabel.Text == Text
+end
+
+ChatStart:FireServer(workspace.FriendlyNPCs["Hair Stylist"])
+repeatAdvance("Alright", {"Yes"})
+
+HairScript = game.Players.LocalPlayer.Backpack:WaitForChild("HairScript", 5)
+HairScript.RemoteEvent:FireServer("woah")
+
+game.Players.LocalPlayer.CharacterAdded:Wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(formerPosition)
