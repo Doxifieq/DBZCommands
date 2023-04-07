6e7a9434d6a1bbbe373bbe27448f57ddbcffd00a
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-07 23:11:29 +0200

    Update dtglitch.lua

--- Commands/dtglitch.lua
+++ Commands/dtglitch.lua
@@ -1,1 +1,11 @@
+local DragonThrow = game.Players.LocalPlayer.Backpack:FindFirstChild("Dragon Throw") or game.Players.LocalPlayer.Character:FindFirstChild("Dragon Throw")
 
+if DragonThrow then
+	DragonThrow.Parent = Player.Character
+
+	local Flip = DragonThrow.Activator:FindFirstChild("Flip")
+
+	if Flip then
+		Flip:Destroy()
+	end
+end
