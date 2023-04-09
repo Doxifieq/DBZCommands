85de02a585b02566b223b151d437c50fdc8f3fe8
Doxifieq <94010949+Doxifieq@users.noreply.github.com>
2023-04-09 16:43:51 +0200

    Update deletewormhole.lua

--- Commands/deletewormhole.lua
+++ Commands/deletewormhole.lua
@@ -1,5 +1,9 @@
 local Wormhole = workspace:FindFirstChild("Wormhole")
 
 if Wormhole then
-	Wormhole:Destroy()
+	if Wormhole.Position ~= Vector3.new(2656.72, 3945.04, -2516.38) then
+		Wormhole.Position = Vector3.new(2656.72, 3945.04, -2516.38)
+	else
+		Wormhole.Position = Vector3.new(2656.72, 5000.04, -2516.38)
+	end
 end
